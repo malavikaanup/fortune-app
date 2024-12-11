d7c60848781ff1939552057c60a8b71475732bb9
Malavika Akhilesh <malavikaakhilesh@Malavikas-MBP-2.cg.shawcable.net>
2024-12-11 14:58:50 -0500

    Refactored using Providers

--- fortune_cookie/lib/main.dart
+++ fortune_cookie/lib/main.dart
@@ -1,9 +1,12 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:fortune_cookie/providers/FortuneModel.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+      create: (BuildContext context) => FortuneModel(), child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -23,7 +26,58 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// Using Providers
 class MyHomePage extends StatefulWidget {
+  MyHomePage({super.key});
+
+  // ignore: unused_field
+  final String _currentFortune = "";
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final fortune = Provider.of<FortuneModel>(context);
+
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              'assets/images/fortune_cookie.png',
+              width: 200,
+              height: 200,
+              fit: BoxFit.cover,
+            ),
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  fortune.currentFortune,
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () => fortune.getNewFortune,
+                child: const Text('Get Fortune'))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+//Simple State Management
+/*class MyHomePage extends StatefulWidget {
   MyHomePage({super.key});
 
   // ignore: unused_field
@@ -84,4 +138,4 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
-}
+}*/
--- fortune_cookie/lib/providers/FortuneModel.dart
+++ fortune_cookie/lib/providers/FortuneModel.dart
@@ -1,3 +1,35 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class FortuneModel extends ChangeNotifier {}
+class FortuneModel extends ChangeNotifier {
+  String _currentFortune = "";
+
+  final _fortuneList = [
+    "You will find a new friend.",
+    "You will find a new friend tomorrow.",
+    "A truly rich life contains love and art in abundance.",
+    "Accept something that you cannot change, and you will feel better.",
+    "Adventure can be real happiness.",
+    "Advice is like kissing. It costs nothing and is a pleasure.",
+    "Advice, when most needed, is least heeded."
+  ];
+
+  String get currentFortune => _currentFortune;
+
+  FortuneModel() {
+    _getRandomFortune();
+  }
+
+  void _getRandomFortune() {
+    var random = Random();
+    int fortuneIndex = random.nextInt(_fortuneList.length);
+    _currentFortune = _fortuneList[fortuneIndex];
+    notifyListeners();
+  }
+
+  //Method to get a new random fortune
+  void getNewFortune() {
+    _getRandomFortune();
+  }
+}
